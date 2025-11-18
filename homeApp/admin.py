b43164353431d0cc96be33425b11062d77a70675
pawancodecrafts <kpawanpreet2003@gmail.com>
2025-11-18 11:12:33 +0530

    models

--- homeApp/admin.py
+++ homeApp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+# from .models import modelkanaams
+# admin.site.register(modelkanaams)
+
+from .models import student
+
+
+admin.site.register(student)
+
--- homeApp/models.py
+++ homeApp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class student(models.Model):
+    roll_No = models.IntegerField()
+    name = models.CharField(max_length=50)
+    desc = models.TextField()
+    isActive = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return f'{self.roll_No} --> {self.name}'
+    
+    
+    # unique --> data unique ho
+    # default --->data default lene ke liye
+    # blank=True ---> khali field rakhni ki nhi 
